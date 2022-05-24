508443ce50b915a6646cb555ef3e0a4b0b194b03
Quoc Dung Le <dunglq.contact@gmail.com>
2022-05-24 15:19:52 +0700

    15:20:00.24-05-2022.Adding get api for store product detail

---
+++ LOSMST.API/Controllers/StoreProductDetailController.cs
@@ -0,0 +1,36 @@
+﻿using LOSMST.Business.Service;
+using LOSMST.Models.Helper;
+using LOSMST.Models.Helper.DBOHelper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LOSMST.API.Controllers
+{
+    [Route("api/store-product-details")]
+    [ApiController]
+    public class StoreProductDetailController : ControllerBase
+    {
+        private readonly StoreProductDetailService _storeProductDetailService;
+
+        public StoreProductDetailController(StoreProductDetailService storeProductDetailService)
+        {
+            _storeProductDetailService = storeProductDetailService;
+        }
+        [HttpGet]
+        public IActionResult GetAllStoreProductDetail([FromQuery] StoreProductDetailParameter storeProductDetailParam, [FromQuery] PagingParameter paging)
+        {
+            var data = _storeProductDetailService.GetAllStoreProductDetail(storeProductDetailParam, paging);
+            var metadata = new
+            {
+                data,
+                data.TotalCount,
+                data.PageSize,
+                data.CurrentPage,
+                data.TotalPages,
+                data.HasNext,
+                data.HasPrevious
+            };
+            return Ok(metadata);
+        }
+    }
+}
---
+++ LOSMST.Business/Service/StoreProductDetailService.cs
@@ -0,0 +1,66 @@
+﻿using LOSMST.DataAccess.Repository.IRepository.DatabaseIRepository;
+using LOSMST.Models.Database;
+using LOSMST.Models.Helper;
+using LOSMST.Models.Helper.DBOHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOSMST.Business.Service
+{
+    public class StoreProductDetailService
+    {
+        private readonly IStoreProductDetailRepository _storeProductDetailRepository;
+
+        public StoreProductDetailService(IStoreProductDetailRepository storeProductDetailRepository)
+        {
+            _storeProductDetailRepository = storeProductDetailRepository;
+        }
+
+        public PagedList<StoreProductDetail> GetAllStoreProductDetail(StoreProductDetailParameter storeProductDetailParam, PagingParameter paging)
+        {
+            var values = _storeProductDetailRepository.GetAll(includeProperties: storeProductDetailParam.includeProperties);
+            if (storeProductDetailParam.Id != null)
+            {
+                values = values.Where(x => x.Id == storeProductDetailParam.Id);
+            }
+            if (storeProductDetailParam.StoreId != null)
+            {
+                values = values.Where(x => x.StoreId == storeProductDetailParam.StoreId);
+            }
+            if (!string.IsNullOrWhiteSpace(storeProductDetailParam.ProductDetailId)){
+                values = values.Where(x => x.ProductDetailId == storeProductDetailParam.ProductDetailId); 
+            }
+            if (!string.IsNullOrWhiteSpace(storeProductDetailParam.sort))
+            {
+                switch (storeProductDetailParam.sort)
+                {
+                    case "id":
+                        if(storeProductDetailParam.dir == "asc")
+                            values = values.OrderBy(x => x.Id);
+                        else if(storeProductDetailParam.dir == "desc")
+                            values.OrderByDescending(x => x.Id);
+                        break;
+                    case "productDetailId":
+                        if(storeProductDetailParam.dir == "asc")
+                            values = values.OrderBy(values => values.ProductDetailId);
+                        else if(storeProductDetailParam.dir=="desc")
+                            values = values.OrderByDescending(values => values.ProductDetailId);
+                        break;
+                    case "storeId":
+                        if (storeProductDetailParam.dir == "asc")
+                            values = values.OrderBy(values => values.StoreId);
+                        else if (storeProductDetailParam.dir == "desc")
+                            values = values.OrderByDescending(values => values.StoreId);
+                        break;
+                }
+            }
+
+            return PagedList<StoreProductDetail>.ToPagedList(values.AsQueryable(),
+                paging.PageNumber,
+                paging.PageSize);
+        }
+    }
+}
